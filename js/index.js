1012cadf3c923f838a4451a675d1c51dfeba002c
Nicolas Vauché <hello@nicolasvauche.net>
2021-05-01 17:09:22 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,91 @@
+const url = 'http://localhost:8000/api'
+
+const getFirstApiAnswer = () => {
+  window
+    .fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        showError(response)
+      } else {
+        response.json().then(response => {
+          showAnswer(response)
+        })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
+
+const showError = error => {
+  const mainElt = document.querySelector('html > body > main')
+
+  const errorElt = document.createElement('p')
+  errorElt.classList.add('error')
+
+  switch (error.status) {
+    case 404:
+      errorElt.innerHTML = 'Ooops…<br />' + error.statusText
+      break
+    default:
+      errorElt.innerHTML = error
+      break
+  }
+
+  mainElt.innerHTML = ''
+  mainElt.appendChild(errorElt)
+}
+
+const showAnswer = data => {
+  console.log(data)
+  for (let i = 0; i < data.length; i++) {
+    buildPost(data[i])
+  }
+}
+
+const buildPost = post => {
+  const postsGrid = document.getElementById('posts')
+
+  const postThumbnail = document.createElement('div')
+  postThumbnail.classList.add('grid-item')
+
+  const thumbnailHeader = document.createElement('div')
+  thumbnailHeader.classList.add('grid-item-header')
+
+  const thumbnailContent = document.createElement('div')
+  thumbnailContent.classList.add('grid-item-content')
+
+  const postMedia = document.createElement('img')
+  postMedia.src = post.image
+  postMedia.alt = post.title
+  postMedia.height = 250
+  postMedia.classList.add('grid-item-img')
+  postMedia.classList.add('top')
+  thumbnailHeader.appendChild(postMedia)
+
+  const postTitle = document.createElement('h3')
+  postTitle.innerHTML = post.title
+  postTitle.classList.add('grid-item-title')
+  thumbnailHeader.appendChild(postTitle)
+
+  const postExcerpt = document.createElement('p')
+  postExcerpt.innerHTML = truncate(post.content, 125)
+
+  postExcerpt.classList.add('grid-item-excerpt')
+  thumbnailContent.appendChild(postExcerpt)
+
+  postThumbnail.appendChild(thumbnailHeader)
+  postThumbnail.appendChild(thumbnailContent)
+  postsGrid.appendChild(postThumbnail)
+}
+
+const truncate = (text, limit) => {
+  if (text.length > limit) {
+    return text.substring(0, limit) + '…'
+  }
+  return text
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  getFirstApiAnswer()
+})
